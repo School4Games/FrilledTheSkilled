90e2b90cfadbcdd6629c985b79b557a686adccd4
TheBigD <otakulp@gmx.de>
2014-10-29 12:20:10 +0100

    new branch start

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
 	public float speed = 1.0f;
-	void FixedUpdate(){
-		if (Input.GetKey (KeyCode.A)) {
+	void FixedUpdate()
+	{
+		if (Input.GetKey (KeyCode.A))
+		{
 			this.transform.rigidbody2D.AddForce (new Vector2 (-speed, 0));
-		} else if (Input.GetKey (KeyCode.D)) {
+		}
+		else if (Input.GetKey (KeyCode.D))
+		{
 			this.transform.rigidbody2D.AddForce (new Vector2 (speed, 0));
 		}
 		
-		if(Input.GetKeyDown (KeyCode.Space)){
-			this.transform.rigidbody2D.AddForce(new Vector2(0,100.0f));}
+		if(Input.GetKeyDown (KeyCode.Space))
+		{
+			this.transform.rigidbody2D.AddForce(new Vector2(0,100.0f));
+		}
 	}
 }
