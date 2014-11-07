ffa2bf836bf30439bb97b1acd249c681ba447060
TheBigD <otakulp@gmx.de>
2014-11-07 15:03:04 +0100

    07.10 update
    updates Move script
    Background changes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,20 +4,20 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
 	public float speed = 1.0f;
-	void FixedUpdate()
-	{
-		if (Input.GetKey (KeyCode.A))
-		{
-			this.transform.rigidbody2D.AddForce (new Vector2 (-speed, 0));
-		}
-		else if (Input.GetKey (KeyCode.D))
-		{
-			this.transform.rigidbody2D.AddForce (new Vector2 (speed, 0));
+	void FixedUpdate(){
+		if (Input.GetKey (KeyCode.A)) {
+			this.transform.rigidbody2D.AddForce (new Vector2 (-speed, 1));
+		} else if (Input.GetKey (KeyCode.D)) {
+			this.transform.rigidbody2D.AddForce (new Vector2 (speed, 1));
 		}
 		
-		if(Input.GetKeyDown (KeyCode.Space))
-		{
-			this.transform.rigidbody2D.AddForce(new Vector2(0,100.0f));
-		}
+		if(Input.GetKeyDown (KeyCode.Space)){
+			this.transform.rigidbody2D.AddForce(new Vector2(0,100.0f));}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+						this.transform.rigidbody2D.AddForce (new Vector3 (0, 100.0f));
+				}
+
+				
 	}
 }
