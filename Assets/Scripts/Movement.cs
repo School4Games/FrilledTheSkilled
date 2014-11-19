43b73a9a2bc89ac051cf44a42e6ba2f65d3ca315
TheBigD <otakulp@gmx.de>
2014-11-19 19:09:11 +0100

    Update 19.11

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,20 +4,33 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
 	public float speed = 1.0f;
+	public float rayLength = 1.5f;
+	public float jumpSpeed = 15.0f;
+	public AnimationCurve PlayerSpeed;
+
+	private LayerMask mask = 8;
+
+	public bool isGrounded()
+	{
+		Vector2 myPos = new Vector2 (this.transform.position.x, this.transform.position.y); // Jump Fixierung 
+		return Physics2D.Raycast(myPos, -Vector2.up, rayLength, 1<<mask.value);
+	}
+	
 	void FixedUpdate(){
+
 		if (Input.GetKey (KeyCode.A)) {
-			this.transform.rigidbody2D.AddForce (new Vector2 (-speed, 1));
+			this.transform.rigidbody2D.AddForce (new Vector2 (-speed, 0)); // Der Spieler bewegt sich nach Links
 		} else if (Input.GetKey (KeyCode.D)) {
-			this.transform.rigidbody2D.AddForce (new Vector2 (speed, 1));
+			this.transform.rigidbody2D.AddForce (new Vector2 (speed, 0)); // Der Spieler bewegt sich nach Rechts 
 		}
 		
-		if(Input.GetKeyDown (KeyCode.Space)){
-			this.transform.rigidbody2D.AddForce(new Vector2(0,100.0f));}
-
-		if (Input.GetKeyDown (KeyCode.Space)) {
-						this.transform.rigidbody2D.AddForce (new Vector3 (0, 100.0f));
-				}
-
-				
+		if (isGrounded ()) {
+			if (Input.GetKey (KeyCode.Space) || Input.GetKey ("w")) { // Der Spieler kann mit dem Code Springen
+				//Dann verändert sich der Y Wert des Spielers
+				this.rigidbody2D.velocity = new Vector3 (0, jumpSpeed, 0);
+			}
+		}
 	}
+
+
 }
