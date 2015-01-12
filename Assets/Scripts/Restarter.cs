827a1e89c9a43fccb33efbf6810a2f664efc9215
TheBigD <otakulp@gmx.de>
2015-01-12 15:22:06 +0100

    12.01 New Level 01
    Neuer Level 01
    Assets eingebaut
    Beleuchtung

--- Assets/Scripts/Restarter.cs
+++ Assets/Scripts/Restarter.cs
@@ -6,6 +6,7 @@ public class Restarter : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if(other.tag == "Player")
-			Application.LoadLevel(Application.loadedLevelName);
+			Application.LoadLevel(Application.loadedLevel);
+		Debug.Log ("Hello world ");
 	}
 }
---
+++ Assets/Scripts/Test scripts/BoxTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Play animation on Trigger Enter
+public class BoxTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
